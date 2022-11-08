65c24ec5d01ad55681c066a2226516a1cd241c15
Edwin Brandon Ortega <72614812+ebor94@users.noreply.github.com>
2022-11-08 07:52:22 -0500

    boton añadir producto y modal de  detalle ok

--- src/modal/modalAddCart.js
+++
@@ -1,4 +0,0 @@
-import { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
--- src/products/BtnAgregarProd.js
+++ src/products/BtnAgregarProd.js
@@ -1,10 +1,83 @@
-import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Figure from "react-bootstrap/Figure";
+import { getPrice } from "../data/products";
+ import Container from "react-bootstrap/Container";
+ import Row from "react-bootstrap/Row";
+ import Col from "react-bootstrap/Col";
 
+function Addtocart(props) {
+    const [show, setShow] = useState(false);
+    const [price, setPrice] = useState(0);
 
-function Addtocart(props){
-    return ( 
-         <Button variant="danger rounded-0">Añadir Producto</Button>
-         )
+  const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+         getPrice(props.MATERIAL).then((price) => {
+           setPrice(price);
+          
+         });
+       
+    }); 
+    
+    const AddDetail = () => ({
+     
+ })   
+
+  return (
+    <>
+      <Button variant="danger  rounded-0" onClick={handleShow}>
+        Agregar Producto
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="text-center">{props.DESCRIPCION}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col md={{ offset: 3 }}>
+                <Figure>
+                  <Figure.Image
+                    width={171}
+                    height={250}
+                    alt={props.DESCRIPCION}
+                    src={`https://www.ceramicaitalia.com/temporada/${props.MATERIAL}.jpg`}
+                  />
+                </Figure>
+              </Col>
+            </Row>
+          </Container>
+
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Precio</Form.Label>
+              <Form.Control type="text" disabled value={`$${price}`} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+              <Form.Label>Disponible</Form.Label>
+              <Form.Control type="text" disabled value={props.CANTIDAD} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Ingrese Cantidad</Form.Label>
+              <Form.Control type="text" autoFocus />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={AddDetail}>
+            Agregar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
-
-export default Addtocart
+export default Addtocart;
