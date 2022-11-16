83cd0f5ad6abeb5e6008f7977e5d9c508db0c36a
Edwin Brandon Ortega <72614812+ebor94@users.noreply.github.com>
2022-11-16 14:46:09 -0500

    form component city

--- src/modal/components/city.js
+++ src/modal/components/city.js
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
-import {getCity} from "../../data/location"
+import { getCity } from "../../data/location";
 
 function GetCitys() {
-    
-    const [citys, Setcitys] = useState([]);
+  const [citys, Setcitys] = useState([]);
 
-    useEffect(() => {
-        getCity("54").then((citys) => Setcitys(citys));
-    })
-    
-    return (
-      <>
-        {/* <Form.Group className="mb-3" controlId="departamento">
-          <Form.Label>Ciudad</Form.Label>
-          <Form.Select id="departamento">
-            {citys.map(({ CITY_CODE, CITY_NAME }, index) => {
-              return <option value={CITY_CODE}>{CITY_NAME}</option>;
-            })}
-          </Form.Select>
-        </Form.Group> */}
-      </>
-    );
-
+  useEffect(() => {
+    getCity("54").then((citys) => Setcitys(citys));
+  });
+  return (
+    <>
+      <Form.Group className="mb-3" controlId="departamento">
+        <Form.Label>Ciudad</Form.Label>
+        <Form.Select id="departamento">
+          {citys.map(({ CITY_CODE, CITY_NAME }, index) => {
+            return <option value={CITY_CODE}>{CITY_NAME}</option>;
+          })}
+        </Form.Select>
+      </Form.Group>
+    </>
+  );
 }
- 
 
 export default GetCitys;
