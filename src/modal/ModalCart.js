8234da4fa43e4cf85eefe1b7dd262f73647003c5
Edwin Brandon Ortega <72614812+ebor94@users.noreply.github.com>
2022-12-02 11:15:11 -0500

    modal getcart

--- src/modal/ModalCart.js
+++ src/modal/ModalCart.js
@@ -2,17 +2,17 @@ import Modal from "react-bootstrap/Modal";
 import React, { useEffect, useState } from "react";
 function ModalCart(props) {
   const [show, setShow] = useState(false);
-   const [cart, Setcart] = useState(props.cart);
+  const [cart, Setcart] = useState(props.cart);
 
   const handleShow = () => {
     setShow(props.show);
   };
 
   const handleClose = () => {
-      setShow(false);
+    setShow(false);
   };
-  
-   console.log(cart);
+
+  //  console.log(cart);
   return (
     <>
       <Modal
@@ -24,9 +24,7 @@ function ModalCart(props) {
         <Modal.Header closeButton>
           <Modal.Title> Detalle De su Pedido</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          {cart}
-        </Modal.Body>
+        <Modal.Body>{cart}</Modal.Body>
       </Modal>
     </>
   );
--- src/products/AddDetail.js
+++ src/products/AddDetail.js
@@ -1,46 +1,41 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
-import {AddDetail } from  "../data/products"
+import { AddDetail } from "../data/products";
 import { Getdetails } from "../data/products";
 import ModalCart from "../modal/ModalCart";
 
-
 function LoadingButton(props) {
   //material, description, price, quantity, discount
- 
-    const [isLoading, setLoading] = useState(false);
-    const [message, setMessage] = useState("Añadir Producto")
-    const [variant, setVariant] = useState("danger")
-    const [cart, Setcart] = useState([]);
-    const [show, setShow] = useState(false);
-  
-    
+
+  const [isLoading, setLoading] = useState(false);
+  const [message, setMessage] = useState("Añadir Producto");
+  const [variant, setVariant] = useState("danger");
+  const [cart, Setcart] = useState([]);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
-      if (isLoading) {
-        //console.log(props)
-        AddDetail(props.code, props.name, props.price, props.quantity, 0)
-          .then((res) => {
-            setMessage(res);
-            setVariant("success");
-              setLoading(false);
-            if (res === "REGISTRO ACTUALIZADO") {
-              Getdetails().then((cart) => Setcart(cart));
-              setTimeout(props.handleClose(), 2000);
-              setShow(true);
-              
-              }
-                       
-               
-          });
-          
+    if (isLoading) {
+      //console.log(props)
+      AddDetail(props.code, props.name, props.price, props.quantity, 0).then(
+        (res) => {
+          setMessage(res);
+          setVariant("success");
+          setLoading(false);
+          if (res === "REGISTRO ACTUALIZADO") {
+            Getdetails().then((cart) => Setcart(cart));
+            setTimeout(props.handleClose(), 2000);
+            setShow(true);
+          }
+        }
+      );
     }
   }, [isLoading]);
 
-    const handleClick = () => {
-        setLoading(true)
+  const handleClick = () => {
+    setLoading(true);
+  };
 
-    };
+  console.log(cart);
 
   return (
     <fragment>
@@ -56,5 +51,4 @@ function LoadingButton(props) {
   );
 }
 
-
-export default LoadingButton
+export default LoadingButton;
