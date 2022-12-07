bdff6d331ddcf502b7d52caf8a4deaad94e37a78
Edwin Brandon Ortega <72614812+ebor94@users.noreply.github.com>
2022-12-07 11:06:50 -0500

    top-fixed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Getproducts from "./products/product";
 import ModalParametros from "./modal/modalParametros";
 
 function App() {
-
-  
   return (
     <div className="App">
-      <nav class="navbar navbar-default navbar-fixed-top">
-        <img src={logo} className="App-logo" alt="logo" />
+      <nav class="navbar bg-danger navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+              <img src={logo} className="App-logo" alt="logo" />
+            </a>
+          </div>
+        </div>
       </nav>
       <ModalParametros />
       <Getproducts />
